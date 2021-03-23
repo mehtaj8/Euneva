a4f42efefb0f037c6e9c938394b984f96fe6e278
Zack Ren <renx11@mcmaster.ca>
2021-03-23 12:03:06 -0400

    Revert "create fields for aveune login"
    This reverts commit bc0b8eaba057933fc04165fd386fa61048c1a5da

--- code/client/src/Components/TodoListComponentFolder/TodoList.tsx
+++ code/client/src/Components/TodoListComponentFolder/TodoList.tsx
@@ -7,8 +7,7 @@ import './TodoList.css';
 
 export const TodoList: FunctionComponent<ITodoListProps> = (props: ITodoListProps) => {
   const [todoItemsCollection, setTodoItemsCollection] = useState(props.todoItemsCollection);
-  const [username, setUsername] = useState('');
-  const [password, setPassword] = useState('');
+
   // Function to update the TodoItem information in local state & database
   const updateTodoItemTitleHandler = async (
     event: React.KeyboardEvent<HTMLInputElement>,
@@ -91,24 +90,6 @@ export const TodoList: FunctionComponent<ITodoListProps> = (props: ITodoListProp
   return (
     <div className='root-todolist-container'>
       <div className='todolist-buttons-container'>
-        <form>
-          <input
-            type='text'
-            id='username'
-            name='username'
-            className='login-field'
-            placeholder='MAC ID'
-            onChange={event => setUsername(event.target.value)}></input>
-          <input
-            type='password'
-            id='password'
-            className='login-field'
-            placeholder='Password'
-            onChange={event => setPassword(event.target.value)}></input>
-          <button type='submit' className='add-todoitem-button'>
-            Connect to Avenue
-          </button>
-        </form>
         {renderAddItemButton()}
         <button className={'add-todoitem-button'} onClick={props.createTodoListHandler}>
           Create New List
@@ -169,25 +150,6 @@ const createTodoItemRequest = async (_listId: string) => {
         _listId: _listId,
         title: ' ',
         description: ' ',
-      }),
-    });
-    const { data } = await response.json();
-    return data;
-  } catch (error) {
-    console.log(error);
-    return error;
-  }
-};
-
-const getUserTodoListRequest = async (_username: string, _password: string) => {
-  const endpoint = API + `user/`;
-  try {
-    const response: Response = await fetch(endpoint, {
-      method: 'POST',
-      body: JSON.stringify({
-        _username: _password,
-        username: ' ',
-        password: ' ',
       }),
     });
     const { data } = await response.json();
